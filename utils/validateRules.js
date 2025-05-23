986b2de47e24ddd92e7c50815cf978a0692d21b0
Tau Hsu <jason850629@gmail.com>
2025-05-23 20:02:41 +0800

    utils/validateRules.js 更新：新增 COUPONS_RULES
    utils/validateRules.js 更新：新增 COUPONS_RULES

---
+++ utils/validateRules.js
@@ -0,0 +1,12 @@
+const COUPONS_RULES = {
+  code: "string",
+  name: "string",
+  discount: "number",
+  quantity: "number",
+  distributedQuantity: "number",
+  startAt: "string",
+  endAt: "string",
+  isAvailable: "boolean",
+};
+
+module.exports = { COUPONS_RULES };
