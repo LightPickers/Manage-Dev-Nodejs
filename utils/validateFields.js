7d20db3bcd74d11b6292fccca97d1f1de780e453
Tau Hsu <jason850629@gmail.com>
2025-05-29 03:33:18 +0800

    utils/validateFields.js Update
    utils/validateFields.js Update

--- utils/validateFields.js
+++ utils/validateFields.js
@@ -6,6 +6,7 @@ const {
   isValidBoolean,
 } = require("./validUtils");
 
+// 驗證欄位
 function validateFields(fields, rules) {
   const errors = [];
   for (const [key, expectedType] of Object.entries(rules)) {
@@ -14,27 +15,14 @@ function validateFields(fields, rules) {
     // 檢查是否有值
     if (isUndefined(value)) {
       errors.push(`${key} ${ERROR_MESSAGES.FIELDS_INCORRECT}`);
-      continue;
-    }
-    // 檢查型別
-    if (typeof value !== expectedType) {
-      errors.push(`${key} 必須是 ${expectedType}`);
-      continue;
-    }
-    // 檢查字串
-    if (expectedType == "string" && !isValidString(value)) {
-      errors.push(`${key} ${ERROR_MESSAGES.FIELDS_INCORRECT}`);
-      continue;
-    }
-    //檢查整數
-    if (expectedType == "number" && !isValidInteger(value)) {
-      errors.push(`${key} ${ERROR_MESSAGES.FIELDS_INCORRECT}`);
-      continue;
-    }
-    //檢查布林值
-    if (expectedType == "boolean" && !isValidBoolean(value)) {
-      errors.push(`${key} ${ERROR_MESSAGES.FIELDS_INCORRECT}`);
-      continue;
+    } else if (typeof value !== expectedType) {
+      errors.push(`${key} 必須是 ${expectedType}`); // 檢查型別
+    } else if (expectedType == "string" && !isValidString(value)) {
+      errors.push(`${key} ${ERROR_MESSAGES.FIELDS_INCORRECT}`); // 檢查字串
+    } else if (expectedType == "number" && !isValidInteger(value)) {
+      errors.push(`${key} ${ERROR_MESSAGES.FIELDS_INCORRECT}`); //檢查整數
+    } else if (expectedType == "boolean" && !isValidBoolean(value)) {
+      errors.push(`${key} ${ERROR_MESSAGES.FIELDS_INCORRECT}`); //檢查布林值
     }
   }
   return errors.length > 0 ? errors : null;
