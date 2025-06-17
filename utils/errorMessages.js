e587b25392775d2bb963ef251ce5195b12c16e45
Tau Hsu <jason850629@gmail.com>
2025-06-17 10:18:34 +0800

    update utils/errorMessages.js
    新增 ORDER_STATUS_NOT_RULE

--- utils/errorMessages.js
+++ utils/errorMessages.js
@@ -52,6 +52,7 @@ const ERROR_MESSAGES = {
   PRODUCT_DELETED: "商品已刪除",
   SEARCH_FORMAT_FAILED: "篩選格式錯誤",
   PRODUCT_SHELVED: "商品已上架",
+  ORDER_STATUS_NOT_RULE: "訂單狀態不符規則，必須為 paid, pending, canceled",
 };
 
 module.exports = ERROR_MESSAGES;
